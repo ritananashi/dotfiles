888b9cf6c0e81b27759e65fba0fc499335aa81ed
ritananashi <deizu573@gmail.com>
2025-06-26 21:55:06 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # 配置したい設定ファイル
 dotfiles=(.zshrc .tmux.conf)
+nvim=(.github/ doc/ lua/ .gitignore .stylua.tomol LICENSE.md README.md init.lua)
 
 # .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
         ln -svf ~/dotfiles/${file} ~/${file}
+done
+
+for file in "${nvim[@]}"; do
+	ln -svf ~/dotfiles/nvim/kickstart.nvim/${file} ~/.config/nvim/${files}
 done
