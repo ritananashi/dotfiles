456bbc317560018417c354aebf16dad41e039e67
ritananashi <deizu573@gmail.com>
2025-07-16 13:28:40 +0900

    add

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,4 +2,9 @@
 alias ls='ls --color'
 alias sz='source ~/.zshrc'
 alias tns='tmux new-session \; source-file ~/dotfiles/.tmux.session.conf'
+alias dcu='docker compose up'
+alias dcd='docker compose down'
+alias dcre='docker compose restart'
+alias dcr='docker compose run'
+alias dc='docker compose'
 
